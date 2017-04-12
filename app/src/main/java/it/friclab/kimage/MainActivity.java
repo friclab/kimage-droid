01ab14196a6d9946f384cdc4b670983035e3558d
fric <silvanoh722@gmail.com>
2017-04-12 21:40:27 +0200

    first commit

---
+++ app/src/main/java/it/friclab/kimage/MainActivity.java
@@ -0,0 +1,38 @@
+package it.friclab.kimage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+
+    private WebView wv1;
+    private String URL = "http://www.kimage.ch/demo/Mendrisio/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        wv1 = (WebView) findViewById(R.id.webview);
+        wv1.setWebViewClient(new WebViewClient());
+
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.loadUrl(URL);
+
+
+    }
+}
